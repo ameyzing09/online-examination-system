500ec3f6d009a0aabbd4aca26f870c9082c76c5d
Harleen Kaur Makhija <hkmakhija26@gmail.com>
2021-10-16 21:59:49 +0530

    my first commit in login branch

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.post('/createTest', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    
+  res.send('Hello from login')  
 })
 
 app.listen(3000, ()=> console.log('Server started at 3000'))
