e1063216a710a61b46810b537f7fca8f2f5556b5
ameyzing09 <55131226+ameyzing09@users.noreply.github.com>
2021-10-20 20:49:42 +0530

    Initial commit

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ let app = express()
 
 app.use(express.json())
 
+app.post('/admin/addTeacher', (req, res) => {
+    let teacherInfo = req.body
+    console.log(`My teacher name is ${teacherInfo.name}`)
+})
+
+
+
 app.post('/createTest', (req, res) => {
     let testDetails = req.body
     console.log(`${JSON.stringify(req.body)}`)
 })
-afhjisdhfjkxzchfgjkl;zxcfnbjklzxbfgjxcbfvjkl;zxnbvkljxcbnv
-zfhKJLZXFN
 
-app.post('/login', (req, res) => {
-  res.send('Hello from login')  
-})
+
+
 
 app.listen(3000, ()=> console.log('Server started at 3000'))
